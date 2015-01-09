caa52210569f39200b56a5778dd9b38bb240e2d3
Amit Saha <amitsaha.in@gmail.com>
2015-01-09 10:18:48 +1000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# pkgquery
-Query for a package/binary across Linux distros
+##Query for a package/binary across Linux distros
+
+**Needs docker up and running on the host**
+
+Examples:
+
+```
+$ python findpkg.py fedora:21 --package flask
+
+============================== N/S matched: flask ==============================
+python-flask-assets.noarch : Asset management for flask
+python-flask-lastuser.noarch : Flask extension for LastUser
+python-flask-script.noarch : Scripting support for Flask
+python-sphinx-theme-flask.noarch : Sphinx Themes for Flask related projects and
+                                 : Flask itself	 
+
+..
+```
+
+```
+> python findpkg.py ubuntu:latest --package flask
+python-flask - micro web framework based on Werkzeug, Jinja2 and good intentions
+python-flask-doc - documentation for Flask micro web framework
+
+```
---
+++ findpkg.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+"""
+
+> python findpkg.py --help
+usage: findpkg.py [-h] [-p PACKAGE] [-b BINARY] image
+
+positional arguments:
+  image                 Linux distro (image:tag)
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -p PACKAGE, --package PACKAGE
+                        Package to search for
+  -b BINARY, --binary BINARY
+                        Binary to search for
+
+
+"""
+
+import sys
+import argparse
+from docker import Client
+
+def search(distro, package=None, binary=None):
+    
+    docker = Client(base_url='unix://var/run/docker.sock')
+    search_pkgcmd = {'fedora':'yum search',
+                     'centos':'yum search',
+                     'ubuntu':'apt-cache search',
+                     'debian':'apt-cache search',
+                 }
+    search_binarycmd = {'fedora':'yum whatprovides',
+                        'centos':'yum whatprovides',
+                        'ubuntu':'apt-file search',
+                        'debian':'apt-file search',
+    }
+    
+    if package:
+        cmd = search_pkgcmd[distro.split(':')[0]]
+        search_for = package
+    if binary:
+        cmd = search_binarycmd[distro.split(':')[0]]
+        search_for = binary
+    contid = docker.create_container(image='{0}'.format(distro),
+                                     command='{0} {1}'.format(cmd, search_for))
+    docker.start(contid)
+    docker.wait(contid)
+    print str(docker.logs(contid))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('image',
+                        help='Linux distro (image:tag)'
+                        )
+    parser.add_argument('-p', '--package',
+                        type=str,
+                        help='Package to search for'
+                        )
+    parser.add_argument('-b', '--binary',
+                        type=str,
+                        help='Binary to search for'
+                        )
+    args = parser.parse_args()
+
+    if len(args.image.split(':')) != 2:
+        sys.exit('Specify image as image:tag')
+
+    package = None
+    binary = None
+    if args.binary:
+        binary = args.binary
+    if args.package:
+        package = args.package
+    if (package and binary) or (not package and not binary):
+        sys.exit('Specify either package or binary')
+    search(args.image, package, binary)
